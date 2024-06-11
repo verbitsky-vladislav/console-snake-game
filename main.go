8a0db6068ec6c74055c9ca7146f13f173544898f
vladislav <blckchvervlad@gmail.com>
2024-06-11 22:08:19 +0300

    v1.0.3: implemented snake self-collision detection leading to game over

--- main.go
+++ main.go
@@ -18,6 +18,7 @@ var (
 	screenWidth  int
 	screenHeight int
 	mu           sync.Mutex
+	gameOver     bool
 )
 
 func initGame() {
@@ -26,26 +27,51 @@ func initGame() {
 	snake = []Point{{screenWidth / 2, screenHeight / 2}}
 	food = generateFood()
 	direction = Point{0, -1}
+	gameOver = false
 }
 
 func generateFood() []Point {
 	foodCount := rand.Intn(10) + 1 // Генерируем от 1 до 10 объектов еды
 	food := make([]Point, foodCount)
 	for i := 0; i < foodCount; i++ {
-		food[i] = Point{rand.Intn(screenWidth), rand.Intn(screenHeight)}
+		food[i] = Point{rand.Intn(screenWidth-2) + 1, rand.Intn(screenHeight-2) + 1}
 	}
 	return food
 }
 
 func draw() {
 	termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
+	drawBorders()
 	for _, p := range snake {
 		termbox.SetCell(p.x, p.y, 'O', termbox.ColorGreen, termbox.ColorDefault)
 	}
 	for _, f := range food {
 		termbox.SetCell(f.x, f.y, 'X', termbox.ColorRed, termbox.ColorDefault)
 	}
+	if gameOver {
+		drawGameOver()
+	}
 	termbox.Flush()
+}
+
+func drawBorders() {
+	for x := 0; x < screenWidth; x++ {
+		termbox.SetCell(x, 0, '#', termbox.ColorWhite, termbox.ColorDefault)
+		termbox.SetCell(x, screenHeight-1, '#', termbox.ColorWhite, termbox.ColorDefault)
+	}
+	for y := 0; y < screenHeight; y++ {
+		termbox.SetCell(0, y, '#', termbox.ColorWhite, termbox.ColorDefault)
+		termbox.SetCell(screenWidth-1, y, '#', termbox.ColorWhite, termbox.ColorDefault)
+	}
+}
+
+func drawGameOver() {
+	message := "Game Over! Press ESC to exit."
+	x := (screenWidth - len(message)) / 2
+	y := screenHeight / 2
+	for i, ch := range message {
+		termbox.SetCell(x+i, y, ch, termbox.ColorRed, termbox.ColorDefault)
+	}
 }
 
 func handleInput() {
@@ -53,26 +79,33 @@ func handleInput() {
 		switch ev := termbox.PollEvent(); ev.Type {
 		case termbox.EventKey:
 			mu.Lock()
-			switch ev.Key {
-			case termbox.KeyArrowUp:
-				if direction.y == 0 {
-					direction = Point{0, -1}
+			if gameOver {
+				if ev.Key == termbox.KeyEsc {
+					termbox.Close()
+					return
 				}
-			case termbox.KeyArrowDown:
-				if direction.y == 0 {
-					direction = Point{0, 1}
+			} else {
+				switch ev.Key {
+				case termbox.KeyArrowUp:
+					if direction.y == 0 {
+						direction = Point{0, -1}
+					}
+				case termbox.KeyArrowDown:
+					if direction.y == 0 {
+						direction = Point{0, 1}
+					}
+				case termbox.KeyArrowLeft:
+					if direction.x == 0 {
+						direction = Point{-1, 0}
+					}
+				case termbox.KeyArrowRight:
+					if direction.x == 0 {
+						direction = Point{1, 0}
+					}
+				case termbox.KeyCtrlC:
+					termbox.Close()
+					return
 				}
-			case termbox.KeyArrowLeft:
-				if direction.x == 0 {
-					direction = Point{-1, 0}
-				}
-			case termbox.KeyArrowRight:
-				if direction.x == 0 {
-					direction = Point{1, 0}
-				}
-			case termbox.KeyCtrlC:
-				termbox.Close()
-				return
 			}
 			mu.Unlock()
 		}
@@ -80,26 +113,38 @@ func handleInput() {
 }
 
 func update() {
+	if gameOver {
+		return
+	}
+
 	head := snake[0]
 	newHead := Point{head.x + direction.x, head.y + direction.y}
 
 	// Обработка переноса на другую сторону экрана
-	if newHead.x >= screenWidth {
-		newHead.x = 0
-	} else if newHead.x < 0 {
-		newHead.x = screenWidth - 1
+	if newHead.x >= screenWidth-1 {
+		newHead.x = 1
+	} else if newHead.x < 1 {
+		newHead.x = screenWidth - 2
 	}
 
-	if newHead.y >= screenHeight {
-		newHead.y = 0
-	} else if newHead.y < 0 {
-		newHead.y = screenHeight - 1
+	if newHead.y >= screenHeight-1 {
+		newHead.y = 1
+	} else if newHead.y < 1 {
+		newHead.y = screenHeight - 2
+	}
+
+	// Проверка на столкновение с собой
+	for _, p := range snake {
+		if p.x == newHead.x && p.y == newHead.y {
+			gameOver = true
+			return
+		}
 	}
 
 	for i, f := range food {
 		if newHead.x == f.x && newHead.y == f.y {
-			food = append(food[:i], food[i+1:]...)                                      // Удалить съеденную еду
-			food = append(food, Point{rand.Intn(screenWidth), rand.Intn(screenHeight)}) // Добавить новую еду
+			food = append(food[:i], food[i+1:]...)                                                  // Удалить съеденную еду
+			food = append(food, Point{rand.Intn(screenWidth-2) + 1, rand.Intn(screenHeight-2) + 1}) // Добавить новую еду
 			goto SkipReduction
 		}
 	}
